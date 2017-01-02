921ac1cb18da15b5a821ffbccc82d5e72e86ef90
fcmam5 <dr.fcmam5@gmail.com>
2017-01-03 00:32:06 +0100

    add a stackoverflow answer

--- ioFiles.h
+++ ioFiles.h
@@ -1,4 +1,6 @@
 #ifndef IO_FILES
 #define IO_FILES
-/*TODO: Add files I/O methodes here*/
+/*TODO: Add files I/O methodes here
+    Get help from: http://stackoverflow.com/a/7856790/5078746
+*/
 #endif
